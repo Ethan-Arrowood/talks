19118c00b3fffc4faffd37a92822e169d4202881
Ethan Arrowood <ethan.arrowood@gmail.com>
2021-02-11 11:01:35 -0500

    complete talk and demo

--- Safely-Handeling-Dynamic-Data-With-TypeScript/demo/src/index.ts
+++ Safely-Handeling-Dynamic-Data-With-TypeScript/demo/src/index.ts
@@ -1,5 +1,13 @@
 import fastify from 'fastify'
+import createServer from './server'
 
 async function run() {
-  
+	const app = fastify()
+	app.register(createServer)
+	await app.ready()
+	const address = await app.listen(0)
+	console.log(`fastify app listening on ${address}`)
+	console.log(app.printRoutes())
 }
+
+run()
--- Safely-Handeling-Dynamic-Data-With-TypeScript/demo/src/server.ts
+++ Safely-Handeling-Dynamic-Data-With-TypeScript/demo/src/server.ts
@@ -1,13 +1,31 @@
 import type { FastifyPluginAsync } from 'fastify'
+import { Static, Type } from '@sinclair/typebox'
 
-const createServer: FastifyPluginAsync = async (server) => {
-  server.route({
-    url: '/',
-    method: 'POST',
-    handler: async (request, reply) => {
+const BodySchema = Type.Object({
+  id: Type.String(),
+  name: Type.String(),
+  employed: Type.Optional(Type.Boolean()),
+  company: Type.Optional(Type.String()),
+  age: Type.Optional(Type.Number()),
+  projects: Type.Optional(
+    Type.Array(Type.String())
+  )
+})
 
+type TBodySchema = Static<typeof BodySchema>;
+
+const createServer: FastifyPluginAsync = async (fastify) => {
+  fastify.post<{
+    Body: TBodySchema
+  }>(
+    '/add-user',
+    { schema: { body: BodySchema } },
+    async (request, reply) => {
+      const { body } = request
+      body.name
+      body.age
     }
-  })
+  )
 }
 
 export default createServer
